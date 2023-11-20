5274faba6bea4641b46ff54e876ae40e4fa463a4
Makhmatkulov <123773249+Makhmatkulov@users.noreply.github.com>
2023-11-21 03:25:33 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,12 +3,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Electronics electronics = new Electronics();
+        Computers computers = new Computers();
+        Sounds sounds = new Sounds();
+        PC pc = new PC();
+        Laptop laptop = new Laptop();
+        Subwoofer subwoofer = new Subwoofer();
+        Loudspeaker loudspeaker = new Loudspeaker();
 
-        Doctor doctor = new Doctor();
-        Xirurg xirurg = new Xirurg();
-        Lor lor = new Lor();
-        System.out.println(xirurg.salary);
-        System.out.println(doctor.job);
-        System.out.println(lor.name);
+        System.out.println(electronics.description);
+        System.out.println();
+        System.out.println(computers.description);
+        System.out.println();
+        System.out.println(sounds.description);
+        System.out.println();
+        System.out.println(pc.description);
+        System.out.println();
+        System.out.println(laptop.description);
+        System.out.println();
+        System.out.println(subwoofer.description);
+        System.out.println();
+        System.out.println(loudspeaker.description);
+        System.out.println();
+
+
     }
 }
